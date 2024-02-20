e0261a39b163f9dd87233b854be62947b7dc75e7
Simone Trevisan <simone.trevisan@ibc.srl>
2024-02-20 19:14:08 +0100

    more oscillators

--- unity-2D-3D/06-3D-project-boost/Assets/Scripts/Oscillator.cs
+++ unity-2D-3D/06-3D-project-boost/Assets/Scripts/Oscillator.cs
@@ -19,14 +19,16 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (period != 0)
+        if (period < Mathf.Epsilon)
         {
-            float cycles = Time.time / period;
-            float rawSinWave = Mathf.Sin(cycles * 2 * Mathf.PI);
-            movementFactor = (rawSinWave + 1f) / 2f;
-
-            Vector3 offset = movementVector * movementFactor;
-            transform.position = startingPosition + offset;
+            return;
         }
+
+        float cycles = Time.time / period;
+        float rawSinWave = Mathf.Sin(cycles * 2 * Mathf.PI);
+        movementFactor = (rawSinWave + 1f) / 2f;
+
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPosition + offset;
     }
 }
