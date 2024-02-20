cf83b67f1adeb77cecaa7e93dff2452a21f0d0cf
Simone Trevisan <simone.trevisan@ibc.srl>
2024-02-20 18:59:52 +0100

    oscillator

---
+++ unity-2D-3D/06-3D-project-boost/Assets/Scripts/Oscillator.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    Vector3 startingPosition;
+    [SerializeField] Vector3 movementVector;
+    float movementFactor;
+    [SerializeField] float period = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float cycles = Time.time / period;
+        float rawSinWave = Mathf.Sin(cycles * 2 * Mathf.PI);
+        movementFactor = (rawSinWave + 1f) / 2f;
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPosition + offset;
+    }
+}
