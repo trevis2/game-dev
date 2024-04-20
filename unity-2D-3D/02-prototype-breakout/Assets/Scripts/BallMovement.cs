f479bb7257d2ccd495522d3b49502a1806fa760d
Simone Trevisan <simone.trevisan@ibc.srl>
2024-04-20 21:39:15 +0200

    tag and adjust movements

--- unity-2D-3D/02-prototype-breakout/Assets/Scripts/BallMovement.cs
+++ unity-2D-3D/02-prototype-breakout/Assets/Scripts/BallMovement.cs
@@ -1,11 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BallMovement : MonoBehaviour
 {
     [SerializeField] GameObject playerPad;
     // Start is called before the first frame update
+    [SerializeField] float ballVelocity = 7.0f;
+    [SerializeField] float angleRotationBump = 10.0f;
     bool isAchoredOnPad = true;
     void Start()
     {
@@ -20,6 +21,10 @@ public class BallMovement : MonoBehaviour
             transform.position = new Vector2(playerPad.transform.position.x, transform.position.y);
         }
 
+        if (Input.GetKeyDown(KeyCode.Space) && isNotInMovement())
+        {
+            LaunchBall(ballVelocity);
+        }
     }
 
     public void LaunchBall(float forceImpulse)
@@ -28,8 +33,27 @@ public class BallMovement : MonoBehaviour
         Vector2 impulse = Vector2.up * forceImpulse; // Impulso lungo l'asse Y
         GetComponent<Rigidbody2D>().AddForce(impulse, ForceMode2D.Impulse);
     }
+
     public bool isNotInMovement()
     {
         return isAchoredOnPad;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Vector2 normale = other.contacts[0].normal * ballVelocity;
+        int randomNumber = Random.Range(0, 2) * 2 - 1;
+        Vector2 normaleRotated = Quaternion.AngleAxis(angleRotationBump * randomNumber, Vector3.back) * normale;
+
+        GetComponent<Rigidbody2D>().AddForce(normaleRotated, ForceMode2D.Impulse);
+        if (other.gameObject.tag == "Block")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.name);
     }
 }
--- unity-2D-3D/02-prototype-breakout/Assets/Scripts/PlayerMovement.cs
+++ unity-2D-3D/02-prototype-breakout/Assets/Scripts/PlayerMovement.cs
@@ -5,31 +5,19 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb;
-    [SerializeField] float velocity = 2.0f;
-    [SerializeField] GameObject ball;
-    [SerializeField] float forceImpulse = 10.0f;
+    [SerializeField] float padVelocity = 350.0f;
+
     private void Awake()
     {
         Physics2D.gravity = Vector2.zero;
         rb = GetComponent<Rigidbody2D>();
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.velocity = new Vector2(Input.GetAxis("Horizontal"), 0) * velocity * Time.fixedDeltaTime;
-        transform.position = new Vector2(Mathf.Clamp(transform.position.x, -7f, 7f), -4.5f);
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal"), 0) * padVelocity * Time.fixedDeltaTime;
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x, -10f, 10f), -4.5f);
     }
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space) && ball.GetComponent<BallMovement>().isNotInMovement())
-        {
-            ball.GetComponent<BallMovement>().LaunchBall(forceImpulse);
-        }
-    }
+
 }
