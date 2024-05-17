59e7c7b5cf5fbee4b833f670765b6113185b3f96
Simone Trevisan <simone.trevisan@ibc.srl>
2024-05-17 21:52:41 +0200

    basic animation and movement

---
+++ unity-2D-3D/08-3D-citybuilder-prototype/Assets/PlayerController.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float collisionOffset = 0.05f;
+    [SerializeField] float movementSpeed = 1.0f;
+    [SerializeField] ContactFilter2D movementFilter;
+    [SerializeField] List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+
+    Animator animator;
+    Vector2 movementInput;
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+
+    void Update()
+    {
+
+    }
+    private void FixedUpdate()
+    {
+        if (movementInput != Vector2.zero)
+        {
+            bool success = TryMove(movementInput);
+            if (!success)
+            {
+                success = TryMove(new Vector2(movementInput.x, 0));
+            }
+            if (!success)
+            {
+                TryMove(new Vector2(0, movementInput.y));
+            }
+            Debug.Log("success:" + success);
+            Debug.Log("move to:" + rb.position + movementInput * movementSpeed * Time.fixedDeltaTime);
+
+            animator.SetBool("isMoving", success);
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
+
+        if (movementInput.x < 0)
+        {
+            sr.flipX = true;
+        }
+        else if (movementInput.x > 0)
+        {
+            sr.flipX = false;
+        }
+        //set direction of sprite
+    }
+
+    private bool TryMove(Vector2 movementInput)
+    {
+        int count = rb.Cast(movementInput, movementFilter, castCollisions, movementSpeed * Time.fixedDeltaTime * collisionOffset);
+
+        if (count == 0)
+        {
+            rb.MovePosition(rb.position + movementInput * movementSpeed * Time.fixedDeltaTime);
+            return true;
+        }
+
+        return false;
+    }
+
+    void OnMove(InputValue movValue)
+    {
+        movementInput = movValue.Get<Vector2>();
+    }
+}
