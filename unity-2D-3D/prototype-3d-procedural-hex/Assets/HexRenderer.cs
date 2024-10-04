932b717cd781aa80ce44a4864388afc0bcab17f4
Simone Trevisan <simone.trevisan@ibc.srl>
2024-10-04 19:42:05 +0200

    doppio esagono

--- unity-2D-3D/prototype-3d-procedural-hex/Assets/HexRenderer.cs
+++ unity-2D-3D/prototype-3d-procedural-hex/Assets/HexRenderer.cs
@@ -35,17 +35,9 @@ public class HexRenderer : MonoBehaviour
     }
     private void Start()
     {
-        // m_faces
-        // Face facciaInferiore = DisegnaFaccia(-heightInferior);
-        DisegnaFaccia(heightSuperior);
+        m_faces = new List<Face>(4);
+        DisegnaFacce();
         ComponiFacce();
-        // m_mesh.vertices = facciaSuperiore.vertices.ToArray();
-        // m_mesh.triangles = facciaSuperiore.triangles.ToArray();
-        // DisegnaFacciaInferiore();
-        // m_mesh.triangles = null;
-        // m_mesh.uv2 = null;
-        m_mesh.RecalculateNormals();
-
     }
 
     private void ComponiFacce()
@@ -55,60 +47,57 @@ public class HexRenderer : MonoBehaviour
         List<Vector2> uvs = new List<Vector2>();
 
 
-        foreach (Face face in m_faces)
+        for (int j = 0; j < m_faces.Count; j++)
         {
-            vertices.AddRange(face.vertices);
-            uvs.AddRange(face.uvs);
-            triangles.AddRange(face.triangles);
+            vertices.AddRange(m_faces[j].vertices);
+            triangles.AddRange(m_faces[j].triangles);
         }
+
+        string verticesString = string.Join(", ", vertices);
+        Debug.Log("vertices: " + verticesString);
+        Debug.Log("Capacity: " + vertices.Capacity);
+        string arrayTriangles = string.Join(", ", triangles);
+        Debug.Log("triangles: " + arrayTriangles);
 
         m_mesh.vertices = vertices.ToArray();
         m_mesh.triangles = triangles.ToArray();
         m_mesh.uv = uvs.ToArray();
+        m_mesh.RecalculateNormals();
     }
 
-    private Face DisegnaFaccia(float heightVertex)
+    private void DisegnaFacce()
     {
 
-        List<Vector3> vertici = CalculateVertexArray(heightVertex);
-        // List<int> triangoli = DisegnaTriangoli();
-
-        // return new Face(vertici, triangoli, new List<Vector2>());
-        return new Face();
+        m_faces.Add(new Face(CalculateVertexArray(heightSuperior), DisegnaTriangoli(0)));
+        m_faces.Add(new Face(CalculateVertexArray(heightInferior), DisegnaTriangoli(1)));
     }
 
     private List<Vector3> CalculateVertexArray(float heightVertex)
     {
         float angle = 360.0f / numberVertex * Mathf.Deg2Rad;
-        List<Vector3> vertices = new List<Vector3>();
-        List<Vector3> vertices2 = new List<Vector3>(numberVertex * 2);
-        vertices2.Insert(0, Vector3.right);
-        vertices.Insert(0, Vector3.right);
-        // vertices2.Insert(2, Vector3.left);
-        // for (int i = 0; i < numberVertex; i++)
-        // {
-        //     float angleInRad = i * angle + GetAngleRotation();
-        //     vertices.Insert(i, new Vector3(innerRange * Mathf.Cos(angleInRad), heightVertex, innerRange * Mathf.Sin(angleInRad)));
-        //     vertices.Insert(i + numberVertex, new Vector3(outerRange * Mathf.Cos(angleInRad), heightVertex, outerRange * Mathf.Sin(angleInRad)));
-        // }
-        string arrayVertices = string.Join(", ", vertices);
-        Debug.Log("vertices: " + arrayVertices);
-        Debug.Log("Capacity: " + vertices.Capacity);
-        string vertices2q = string.Join(", ", vertices2);
-        Debug.Log("vertices2: " + vertices2q);
-        Debug.Log("Capacity: " + vertices2.Capacity);
+
+        List<Vector3> vertices = new List<Vector3>(numberVertex * 2);
+        Vector3[] vertici = new Vector3[numberVertex * 2];
+        for (int i = 0; i < numberVertex; i++)
+        {
+            float angleInRad = i * angle + GetAngleRotation();
+            vertici[i] = new Vector3(innerRange * Mathf.Cos(angleInRad), heightVertex, innerRange * Mathf.Sin(angleInRad));
+            vertici[i + numberVertex] = new Vector3(outerRange * Mathf.Cos(angleInRad), heightVertex, outerRange * Mathf.Sin(angleInRad));
+        }
+
+        vertices.AddRange(vertici);
         return vertices;
     }
 
-    private List<int> DisegnaTriangoli()
+    private List<int> DisegnaTriangoli(int k)
     {
         List<int> triangles = new List<int>();
         for (int i = 0; i < numberVertex; i++)
         {
-            int internalVertex = i;
-            int externalVertex = i + numberVertex;
-            int verticeInternoSuccessivo = (i + 1) % numberVertex;
-            int verticeEsternoSuccessivo = numberVertex + ((i + 1) % numberVertex);
+            int internalVertex = i + 2 * numberVertex * k;
+            int externalVertex = i + numberVertex + 2 * numberVertex * k;
+            int verticeInternoSuccessivo = (i + 2 * numberVertex * k + 1) % (numberVertex + 2 * numberVertex * k);
+            int verticeEsternoSuccessivo = numberVertex + 2 * numberVertex * k + ((i + 1) % numberVertex);
 
             int[] lowTriangle = new int[] { internalVertex, externalVertex, verticeInternoSuccessivo };
             int[] highTriangle = new int[] { verticeInternoSuccessivo, externalVertex, verticeEsternoSuccessivo };
@@ -116,8 +105,7 @@ public class HexRenderer : MonoBehaviour
             int[] triangoli = lowTriangle.Concat(highTriangle).ToArray();
             triangles.AddRange(triangoli);
         }
-        string arrayTriangles = string.Join(", ", triangles);
-        Debug.Log("triangles: " + arrayTriangles);
+
         triangles.Reverse();
         return triangles;
     }
@@ -126,69 +114,17 @@ public class HexRenderer : MonoBehaviour
     {
         return applyRotation ? (angleRotation * Mathf.Deg2Rad) : 0f;
     }
-
-    // private void Update()
-    // {
-    //     angoloFisso = 360.0f / numeroVertici;
-
-    //     m_mesh.vertices = CreazioneVertici(); //creo i vertici prima gli interni e poi gli esterni
-    //     m_mesh.triangles = CreazioneTriangoli(); //la superficie è divisa in 6 facce e ogni faccia composta da 2 triangoli. ogni triangolo ha 3 vertici
-
-    //     m_mesh.RecalculateNormals();
-    // }
-
-    // private int[] CreazioneTriangoli()
-    // {
-    //     int[] triangles = new int[0];
-    //     for (int i = 0; i < numeroVertici; i++)
-    //     {
-    //         int[] faccia = CreaFaccia(i);
-    //         triangles = triangles.Concat(faccia).ToArray();
-    //     }
-    //     string arrayString = string.Join(", ", triangles);
-    //     Debug.Log("triangles: " + arrayString);
-    //     return triangles.Reverse().ToArray();
-    // }
-    // private int[] CreaFaccia(int i)
-    // {
-    //     int verticeInterno = i;
-    //     int verticeEsterno = i + numeroVertici;
-    //     int verticeInternoSuccessivo = (i + 1) % numeroVertici;
-    //     int verticeEsternoSuccessivo = numeroVertici + ((i + 1) % numeroVertici);
-
-    //     int[] triangoloBasso = new int[] { verticeInterno, verticeEsterno, verticeInternoSuccessivo };
-    //     int[] triangoloAlto = new int[] { verticeInternoSuccessivo, verticeEsterno, verticeEsternoSuccessivo };
-    //     int[] faccia = triangoloBasso.Concat(triangoloAlto).ToArray();
-    //     return faccia;
-    // }
-
-    // private Vector3[] CreazioneVertici()
-    // {
-    //     Vector3[] vertices = new Vector3[numeroVertici * 2];
-    //     for (int i = 0; i < numeroVertici; i++)
-    //     {
-    //         float angleInRad = i * angoloFisso * Mathf.Deg2Rad + AngleRotation();
-    //         vertices[i] = new Vector3(innerRange * Mathf.Cos(angleInRad), 0, innerRange * Mathf.Sin(angleInRad));
-    //         vertices[i + numeroVertici] = new Vector3(outerRange * Mathf.Cos(angleInRad), 0, outerRange * Mathf.Sin(angleInRad));
-    //     }
-    //     string arrayString = string.Join(", ", vertices);
-    //     Debug.Log("vertices: " + arrayString);
-    //     return vertices;
-    // }
-
-
-
 }
 
 public struct Face
 {
     public List<Vector3> vertices { get; private set; }
     public List<int> triangles { get; private set; }
-    public List<Vector2> uvs { get; private set; }
-    public Face(List<Vector3> vertices, List<int> triangles, List<Vector2> uvs)
+    //public List<Vector2> uvs { get; private set; }
+    public Face(List<Vector3> vertices, List<int> triangles)//, List<Vector2> uvs)
     {
         this.vertices = vertices;
         this.triangles = triangles;
-        this.uvs = uvs;
+        //this.uvs = uvs;
     }
 }
